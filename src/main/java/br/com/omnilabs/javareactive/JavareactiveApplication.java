a643fa14bdd75bb22df4924a858458263bdce41d
Roger Vieira <rogerdenis@gmail.com>
2018-09-23 23:18:49 -0300

    Added basic structure

---
+++ src/main/java/br/com/omnilabs/javareactive/JavareactiveApplication.java
@@ -0,0 +1,12 @@
+package br.com.omnilabs.javareactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavareactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavareactiveApplication.class, args);
+	}
+}
